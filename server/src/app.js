b5cedd73a470ff34189fe5d27e63fbfb45af2fd6
Joshini04 <joshini042@gmail>
2026-09-16 13:09:34 +0530

    Add cron endpoint for daily reminder emails

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/auth.routes');
 const subscriptionRoutes = require('./routes/subscriptions.routes');
+const { runReminderJob } = require('./utils/reminderJob');
 
 const app = express();
 
@@ -15,6 +16,16 @@ app.use('/api/subscriptions', subscriptionRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'SubTrack API is running' });
+});
+
+app.get('/api/cron/run-reminders', async (req, res) => {
+  const secret = req.query.secret;
+  if (secret !== process.env.CRON_SECRET) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  await runReminderJob();
+  res.json({ message: 'Reminder job executed' });
 });
 
 module.exports = app;
